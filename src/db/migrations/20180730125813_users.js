cf5d677ac0cecadbc504d4382966f088e10a9d67
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2018-07-31 19:43:50 -0400

    add migrations

---
+++ src/db/migrations/20180730125813_users.js
@@ -0,0 +1,14 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', table => {
+    table.increments();
+    table.string('first_name').notNullable();
+    table.string('last_name').notNullable().defaultsTo('');
+    table.string('email').notNullable().unique();
+    table.text('password').notNullable();
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
