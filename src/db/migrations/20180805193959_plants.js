dde6c92b6d35cce8937604b98e818b465794f1cd
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2018-08-06 09:09:52 -0700

    update migrations

---
+++ src/db/migrations/20180805193959_plants.js
@@ -0,0 +1,13 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('plants', table => {
+    table.increments();
+    table.string('title').notNullable();
+    table.text('description').notNullable().defaultsTo('');
+    table.boolean('completed').notNullable().defaultsTo(false);
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('plants');
+};
