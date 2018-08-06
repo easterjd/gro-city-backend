3e90db998f58ce2dfbe4714cd8ebbfc2fae309b8
John Dillon Easter <johndilloneaster@gmail.com>
2018-08-06 14:04:30 -0700

    Fixed user seed issue again

--- src/db/seeds/01_users.js
+++ src/db/seeds/01_users.js
@@ -8,7 +8,7 @@ exports.seed = function (knex, Promise) {
       return knex('users').insert([
         {id: 1, first_name: 'Surya', last_name: 'Moorthy', email: 'ayrus2289@gmail.com', password: hashSync('password')},
         {id: 2, first_name: 'Chris', last_name: 'Peterson', email: 'crpete23@gmail.com', password: hashSync('password')},
-        {id: 3, first_name: 'Dillon', last_name: 'Easter', email: 'johndilloneaster@gmai.com', password: hashSync('password')}
+        {id: 3, first_name: 'Dillon', last_name: 'Easter', email: 'johndilloneaster@gmail.com', password: hashSync('password')}
       ])
     })
     .then(function () {
